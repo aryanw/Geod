2d57846f585013d27d59582f6bd8c395844eb7c4
aryanw <aryanwagadre@gmail.com>
2023-10-28 08:25:17 -0700

    second commit

--- online_marketplace/online_marketplace/urls.py
+++ online_marketplace/online_marketplace/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('core.urls')),
     path('admin/', admin.site.urls),
-    path('items/', include('item.urls'))
+    path('items/', include('item.urls')),
+    path('dashboard/', include('dashboard.urls'))
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
